351b0b9063c1b52b5f201866c00f698822b5e95c
bitmedic <bitmedic01@gmail.com>
2021-04-25 22:53:40 +0200

    Connect ProgressUI with AssemblyManager

--- LD48/Assets/Scripts/UI/ProgressUI.cs
+++ LD48/Assets/Scripts/UI/ProgressUI.cs
@@ -23,11 +23,12 @@ namespace LD48
         public TMPro.TextMeshProUGUI impactProgressText;
 
         public GameObject launchButtonPanel;
+        private AssemblyManager assMan;
 
         // Start is called before the first frame update
         void Start()
         {
-        
+            assMan = GameObject.FindObjectOfType<AssemblyManager>();
         }
 
         // Update is called once per frame
@@ -35,10 +36,13 @@ namespace LD48
         {
             impactTimeLeft = impactTimeTotal - Time.time; //TODO this must be moved to main game loop
 
+            drillProgress = assMan.GetRocket().GetMaterialQuantity("depth");
+
             float drillProgressPct = Mathf.Floor(drillProgress / drillGoal * 100);
             drillProgressBar.rectTransform.sizeDelta = new Vector2(15f, drillProgressPct);
             drillProgressText.text = string.Format("{0,2:00}m", drillProgressPct);
 
+            tntProgress = assMan.GetRocket().GetMaterialQuantity("explosiveness");
             float tntProgressPct = Mathf.Floor(tntProgress / tntGoal * 100);
             tntProgressBar.rectTransform.sizeDelta = new Vector2(15f, tntProgressPct);
             tntProgressText.text = string.Format("{0,2:00}%", tntProgressPct);
